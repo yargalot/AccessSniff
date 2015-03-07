ffa5505306d296c4f05f64d839073499661b697c
Steven John Miller <steven.jmiller@gmail.com>
2015-03-07 19:13:05 +1100

    Use Commander

--- bin/accessSniff.js
+++ bin/accessSniff.js
@@ -1,5 +1,3 @@
 #!/usr/bin/env node
 
-console.log('Access Sniff');
-
 require("../src/cli.js").setup(process.argv);
--- src/cli.js
+++ src/cli.js
@@ -1,17 +1,27 @@
 "use strict";
 
-var accessSniff = require("./accessSniff.js");
+var accessSniff = require('./accessSniff.js');
+var packageInfo = require('../package.json');
+var program = require('commander');
+
 
 var exports = {
   run : function() {
 
   },
 
-  setup: function(files) {
+  setup: function(options) {
 
-    console.log(files);
+    program
+      .version(packageInfo.version)
+      .option('-p, --peppers', 'Add peppers')
+      .option('-P, --pineapple', 'Add pineapple')
+      .option('-b, --bbq', 'Add bbq sauce')
+      .option('-c, --cheese [type]', 'Add the specified type of cheese [marble]', 'marble')
+      .parse(options);
 
-    accessSniff.start(files);
+
+    accessSniff.start(options);
 
   }
 };
