595d0f9d0015b3c8acb220f723833557f5912510
Steve Miller <steven.jmiller@gmail.com>
2016-02-21 20:38:43 +1100

    Test message output
    ref issue #13

--- test/accessSniff.test.js
+++ test/accessSniff.test.js
@@ -50,7 +50,14 @@ exports.accessibilityTests = {
   overall_testString: test => {
     var testString = '<html><body><h1>helloworld<h1></body></html>';
 
-    AccessSniff.default([testString], {force: true})
+    AccessSniff
+      .default([testString], {
+        force: true,
+        ignore: [
+          'WCAG2A.Principle2.Guideline2_4.2_4_2.H25.1.NoTitleEl',
+          'WCAG2A.Principle3.Guideline3_1.3_1_1.H57.2'
+        ]
+      })
       .then(report => {
         test.ok(report[testString], 'Should produce a json report for html string');
         test.expect(1);
