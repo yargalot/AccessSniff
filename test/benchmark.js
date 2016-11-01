3a28c1a6f39206ad91fc603f8897aeae29a13bca
Steve Miller <steven.jmiller@gmail.com>
2016-11-01 15:30:07 +1100

    Benchmark testing

--- test/benchmark.js
+++ test/benchmark.js
@@ -1,18 +1,42 @@
+var AccessSniff = require('../dist');
 var Benchmark = require('benchmark');
 var suite = new Benchmark.Suite;
 
 // add tests
-suite.add('RegExp#test', function() {
-  /o/.test('Hello World!');
+suite
+.add('Phantomjs', {
+  defer: true,
+  fn: (deferred) => {
+    AccessSniff
+      .default('./examples/test.html', {
+        force: true,
+        verbose: false
+      })
+      .then(report => {
+        deferred.resolve();
+      });
+  }
 })
-.add('String#indexOf', function() {
-  'Hello World!'.indexOf('o') > -1;
+.add('JSDom', {
+  defer: true,
+  fn: (deferred) => {
+    AccessSniff
+      .default('./examples/test.html', {
+        force: true,
+        verbose: false,
+        template: true
+      })
+      .then(report => {
+        deferred.resolve();
+      });
+  }
 })
 // add listeners
 .on('cycle', function(event) {
   console.log(String(event.target));
 })
 .on('complete', function() {
+  console.log('Slowest is ' + this.filter('slowest').map('name'));
   console.log('Fastest is ' + this.filter('fastest').map('name'));
 })
 // run async
