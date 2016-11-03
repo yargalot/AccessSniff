f8e38de5b3c80578ba21373fcb7273a8eaf87f29
Steve Miller <steven.jmiller@gmail.com>
2016-11-03 21:38:18 +1100

    Remove comments

--- src/messages/messages.spec.js
+++ src/messages/messages.spec.js
@@ -1,30 +1,7 @@
-
-/*
-  ======== A Handy Little Nodeunit Reference ========
-  https://github.com/caolan/nodeunit
-
-  Test methods:
-    test.expect(numAssertions)
-    test.done()
-  Test assertions:
-    test.ok(value, [message])
-    test.equal(actual, expected, [message])
-    test.notEqual(actual, expected, [message])
-    test.deepEqual(actual, expected, [message])
-    test.notDeepEqual(actual, expected, [message])
-    test.strictEqual(actual, expected, [message])
-    test.notStrictEqual(actual, expected, [message])
-    test.throws(block, [error], [message])
-    test.doesNotThrow(block, [error], [message])
-    test.ifError(value)
-*/
-
 import { ignoredCheck } from './buildMessage';
-
 
 const Tests = {
   setUp: done => {
-    // setup here if necessary
     done();
   },
   isRuleIgnored: test => {
